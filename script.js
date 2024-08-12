a01b4a7bc755719a3385039ecb7c183156df77ce
Parveen Kaur <parveenkaur1045@gmail.com>
2024-08-12 10:27:00 -0600

    add modal

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // alert('hello');
+
+const closeModalBtn = document.getElementById("close_modal");
+const modal =document.getElementById("modal");
+
+closeModalBtn.addEventListener('click',function() {
+    modal.classList.add("hide");
+}
+);
